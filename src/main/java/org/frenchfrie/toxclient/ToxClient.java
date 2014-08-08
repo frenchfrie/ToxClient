8a5f06614f3d3c15c2187ec886f2bf055fcf7372
frenchfrie <>
2014-08-08 16:42:44 +0200

    Natives libs management done at least for developments. Natives are copied in natives folder of artifact The OS/architecture dependent natives folder is also added to path at runtime using System.getProperty() values.

--- src/main/java/org/frenchfrie/toxclient/ToxClient.java
+++ src/main/java/org/frenchfrie/toxclient/ToxClient.java
@@ -20,7 +20,9 @@ import org.frenchfrie.toxclient.ui.MainGui;
 import im.tox.jtoxcore.JTox;
 import im.tox.jtoxcore.ToxException;
 import im.tox.jtoxcore.callbacks.CallbackHandler;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -36,68 +38,92 @@ import javax.swing.SwingUtilities;
  */
 public class ToxClient {
 
-    private MainGui gui;
+   private MainGui gui;
 
-    private JTox tox;
+   private JTox tox;
 
-    private ScheduledThreadPoolExecutor executor;
+   private ScheduledThreadPoolExecutor executor;
 
-    public ToxClient() {
-        try {
-            SwingUtilities.invokeAndWait(new Runnable() {
+   public ToxClient() throws ReflectiveOperationException {
 
-                @Override
-                public void run() {
-                    gui = new MainGui();
-                    gui.pack();
-                    gui.setLocationRelativeTo(null);
-                    gui.setVisible(true);
-                }
-            });
-        } catch (InterruptedException | InvocationTargetException ex) {
-            Logger.getLogger(ToxClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        ToxFriendList fl = new ToxFriendList();
-        CallbackHandler callbackHandler = new CallbackHandler(fl);
-        try {
-            tox = new JTox(fl, callbackHandler);
-            tox.setName("frenchfrie");
-            String address = tox.getAddress();
-            System.out.println("adress = " + address);
+      addDevLibsToPath(System.getProperty("os.name"), System.getProperty("os.arch"));
 
-            executor = new ScheduledThreadPoolExecutor(1);
-            executor.scheduleAtFixedRate(new ToxThread(tox), 50, 50, TimeUnit.MILLISECONDS);
+      try {
+         SwingUtilities.invokeAndWait(new Runnable() {
 
-        } catch (ToxException ex) {
-            Logger.getLogger(ToxClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("path = " + System.getProperty("java.library.path"));
-        new ToxClient();
-    }
-
-    private static class ToxThread implements Runnable {
-
-        private JTox tox;
-
-        public ToxThread(JTox tox) {
-            this.tox = tox;
-        }
-
-        @Override
-        public void run() {
-            try {
-                tox.doTox();
-            } catch (ToxException ex) {
-                Logger.getLogger(ToxClient.class.getName()).log(Level.SEVERE, null, ex);
+            @Override
+            public void run() {
+               gui = new MainGui();
+               gui.pack();
+               gui.setLocationRelativeTo(null);
+               gui.setVisible(true);
             }
-        }
+         });
+      } catch (InterruptedException | InvocationTargetException ex) {
+         Logger.getLogger(ToxClient.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      ToxFriendList fl = new ToxFriendList();
+      CallbackHandler callbackHandler = new CallbackHandler(fl);
+      try {
+         tox = new JTox(fl, callbackHandler);
+         tox.setName("frenchfrie");
+         String address = tox.getAddress();
+         System.out.println("adress = " + address);
 
-    }
+         executor = new ScheduledThreadPoolExecutor(1);
+         executor.scheduleAtFixedRate(new ToxThread(tox), 50, 50, TimeUnit.MILLISECONDS);
+
+      } catch (ToxException ex) {
+         Logger.getLogger(ToxClient.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) throws Exception {
+      System.out.println("path = " + System.getProperty("java.library.path"));
+      new ToxClient();
+   }
+
+   /**
+    * This method allow to add natives libs to the path. It shouldn't be used in
+    * production because a package should depend on natives packages placed at
+    * their proper places on the filesystem (ie /usr/lib/).<br/>
+    * By the time it is packaged, I will add what i can in natives builds
+    * os/arch in folders in natives named &lt;os.name&gt;_&lt;os.arch&gt;.
+    *
+    * @param osName
+    * @param osArch
+    * @throws ReflectiveOperationException
+    */
+   private static void addDevLibsToPath(String osName, String osArch) throws ReflectiveOperationException {
+      String libPathes = System.getProperty("java.library.path");
+      String nativesFolderPath = System.getProperty("user.dir") + "/natives/" + osName + "_" + osArch;
+      System.setProperty("java.library.path", libPathes + ":" + nativesFolderPath);
+      Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
+      fieldSysPath.setAccessible(true);
+      fieldSysPath.set(null, null);
+      fieldSysPath.setAccessible(false);
+   }
+
+   private static class ToxThread implements Runnable {
+
+      private JTox tox;
+
+      public ToxThread(JTox tox) {
+         this.tox = tox;
+      }
+
+      @Override
+      public void run() {
+         try {
+            tox.doTox();
+         } catch (ToxException ex) {
+            Logger.getLogger(ToxClient.class.getName()).log(Level.SEVERE, null, ex);
+         }
+      }
+
+   }
 
 }
